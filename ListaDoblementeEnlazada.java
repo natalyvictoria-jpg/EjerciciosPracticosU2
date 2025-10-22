c13f075eab1e6a3962fdb5904da4d17d630df1f0
naty <1224100674.nvg@gmail.com>
2025-10-21 22:05:45 -0600

    Update ListaDoblementeEnlazada.java

--- ListaDoblementeEnlazada.java
+++ ListaDoblementeEnlazada.java
@@ -1,13 +1,73 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+Actividad: Ejercicios Prácticos – Listas
+Tema: Lista Doblemente Enlazada de Caracteres
+* @author Nataly Victoria Gonzalez Aviles. GTID141
+Fecha: 21/10/2025
  */
 package EjerciciosPracticos;
 
-/**
- *
- * @author Victoria
- */
+import java.util.Scanner;
+
+// Actividad 05: Lista Doblemente Enlazada de Caracteres
+
 public class ListaDoblementeEnlazada {
-    
+    private NodoDoble cabeza;
+
+    public void insertarFinal(char c) {
+        NodoDoble nuevo = new NodoDoble(c);
+        if (cabeza == null)
+            cabeza = nuevo;
+        else {
+            NodoDoble actual = cabeza;
+            while (actual.siguiente != null)
+                actual = actual.siguiente;
+            actual.siguiente = nuevo;
+            nuevo.anterior = actual;
+        }
+    }
+
+    public void ordenar() {
+        if (cabeza == null) return;
+        boolean intercambio;
+        do {
+            intercambio = false;
+            NodoDoble actual = cabeza;
+            while (actual.siguiente != null) {
+                if (actual.caracter > actual.siguiente.caracter) {
+                    char temp = actual.caracter;
+                    actual.caracter = actual.siguiente.caracter;
+                    actual.siguiente.caracter = temp;
+                    intercambio = true;
+                }
+                actual = actual.siguiente;
+            }
+        } while (intercambio);
+    }
+
+    public void mostrar() {
+        NodoDoble actual = cabeza;
+        System.out.print("Lista ordenada: ");
+        while (actual != null) {
+            System.out.print(actual.caracter + " ");
+            actual = actual.siguiente;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ListaDoblementeEnlazada lista = new ListaDoblementeEnlazada();
+
+        System.out.print("Introduce una cadena de texto: ");
+        String texto = sc.nextLine();
+
+        for (char c : texto.toCharArray())
+            lista.insertarFinal(c);
+
+        lista.ordenar();
+        lista.mostrar();
+    }
 }
+
+
+
