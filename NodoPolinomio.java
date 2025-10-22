0ed0d9bafc13f50a5e076fbc890652c677d00a6d
naty <1224100674.nvg@gmail.com>
2025-10-21 22:09:15 -0600

    Update NodoPolinomio.java

--- NodoPolinomio.java
+++ NodoPolinomio.java
@@ -1,13 +1,20 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+Clase NodoPolinomio para representar un término de un polinomio
+Actividad: Ejercicios Prácticos – Listas
+* @author Nataly Victoria Gonzalez Aviles. GTID141
+Fecha: 21/10/2025
  */
 package EjerciciosPracticos;
 
-/**
- *
- * @author Victoria
- */
+// Nodo para representar un término de un polinomio
 public class NodoPolinomio {
-    
+    double coeficiente;
+    int exponente;
+    NodoPolinomio siguiente;
+
+    public NodoPolinomio(double c, int e) {
+        coeficiente = c;
+        exponente = e;
+        siguiente = null;
+    }
 }
