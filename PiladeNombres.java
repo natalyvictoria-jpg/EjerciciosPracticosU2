d38c71288d2863f6a8ab2e2619de1d87b9f75951
naty <1224100674.nvg@gmail.com>
2025-11-04 22:25:08 -0600

    Update PiladeNombres.java

--- PiladeNombres.java
+++ PiladeNombres.java
@@ -16,23 +16,27 @@ package pilas;
 
 import java.util.Scanner;
 
-public class PiladeNombres {
+public class PilaDeNombres {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Pila<String> pila = new Pila<>();
         String nombre;
 
+        // Solicita nombres al usuario hasta que ingrese "FIN"
         while (true) {
             System.out.print("Ingrese un nombre (FIN para salir): ");
             nombre = sc.nextLine();
-            if (nombre.equalsIgnoreCase("FIN")) break;
-            pila.push(nombre);
+            if (nombre.equalsIgnoreCase("FIN")) break; // Sale del ciclo
+            pila.push(nombre); // Agrega el nombre a la pila
         }
 
+        // Imprime los nombres en orden inverso (LIFO)
         System.out.println("Nombres en orden inverso:");
         while (!pila.isEmpty()) {
             System.out.println(pila.pop());
         }
     }
 }
+
+
 
