cf0900e32f9f204d76b5469acd59297def84b189
naty <1224100674.nvg@gmail.com>
2025-11-04 22:23:20 -0600

    Update PilasBasica.java

--- PilasBasica.java
+++ PilasBasica.java
@@ -14,19 +14,25 @@ varios valores enteros, elimina algunos y muestra el contenido final.
 
 package pilas;
 
-public class PilasBasica {
+public class PilaBasica {
     public static void main(String[] args) {
+        // Crea una pila de enteros
         Pila<Integer> pila = new Pila<>();
 
+        // Inserta elementos en la pila
         pila.push(5);
         pila.push(10);
         pila.push(15);
         pila.push(20);
 
+        // Elimina los dos últimos elementos (LIFO)
         pila.pop();
         pila.pop();
 
+        // Muestra el contenido actual de la pila
         System.out.println("Contenido actual de la pila: " + pila);
     }
 }
+
+
 
